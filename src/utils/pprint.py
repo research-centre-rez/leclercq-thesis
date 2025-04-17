15538ea31b12c6a119f9275faad1041a05fef043
Erik Leclercq <rkleclercq@gmail.com>
2025-04-17 12:55:53 +0200

    Modifying pprint.py

--- src/utils/pprint.py
+++ src/utils/pprint.py
@@ -3,18 +3,16 @@ import logging
 """
 This file contains various pretty print methods for different data types
 """
-def pprint_argparse(args, logger=None):
-    if logger is None:
-        logger = logging.getLogger(__name__)
-
+logger = logging.getLogger(__name__)
+def pprint_argparse(args):
     logger.info('Running with the following parameters:')
     for arg in vars(args):
         logger.info('  %s: %s', arg, getattr(args, arg))
 
-def pprint_dict(dict, desc, logger=None):
-    if logger is None:
-        logger = logging.getLogger(__name__)
-
+def pprint_dict(dict_in, desc):
     logger.info(desc)
-    for key, val in dict.items():
-        logger.info(' %s: %s', key, val)
+    for key, val in dict_in.items():
+        if isinstance(val, dict):
+            pprint_dict(val, key.upper())
+        else:
+            logger.info(' %s: %s', key, val)
